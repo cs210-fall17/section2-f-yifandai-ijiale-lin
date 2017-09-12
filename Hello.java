329b78852d135dc9f5b3d36ebcacf90a2638ba24
Jiale Lin <jialelin@email.arizona.edu>
2017-09-11 17:38:13 -0700

    first commit

--- Hello.java
+++ Hello.java
@@ -9,13 +9,15 @@ public class Hello {
     }
 
     public static void callSign() {
-        
+         System.out.println("Lock S-foils in attack position.");
+         System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
 
     public static void goldLeader() {
+        System.out.println("Loosen up!");
 
         // Gold Leader should add a print statement that says "Loosen up!"
     
@@ -23,6 +25,7 @@ public class Hello {
     }
 
     public static void redLeader() {
+        System.out.println("I copy, gold leader!");
  
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
